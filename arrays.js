ef2559cf56bcb684498c3cc930b5b81d365cc5b7
Roundabout Rider <ntm.email@icloud.com>
2018-08-06 12:22:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(foo) {
-  var array = [()]
+  var array = ['array']
   array.push("element")
   return array
 }
